2ac0185d6fed57284f967f89889d1d3f804b2a9f
albhasan <albhasan@gmail.com>
2016-08-23 12:32:58 -0300

    some examples = samples

---
+++ samples.R
@@ -0,0 +1,30 @@
+############################################################################
+# SCIDBUTIL - example
+# 2016-08-23
+############################################################################
+
+library(sp)
+source("scidbUtil.R")
+
+# calculate the size of a pixel
+pixelSize <- .calcPixelSize(4800, .calcTileWidth())
+
+# spatial reference system objects
+proj4326 <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+proj_modis_sinusoidal <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
+
+# create a matrix of points
+x <- c(-11.623990, -11.676781, -11.676781, -11.623990)
+y <- c(-56.325531, -56.325531, -56.239700, -56.239700)
+lonlat.Matrix <- cbind(x, y)
+
+# build spatial-points
+S <- SpatialPoints(lonlat.Matrix)
+proj4string(S) <-CRS(proj4326)
+
+# transforms SRS to MODIS
+lonlat.Matrix.res <- spTransform(S, CRS(proj_modis_sinusoidal))
+
+# transform MODIS to SciDB's col_id, row_id
+res <- .sinusoidal2gmpi(t(bbox(lonlat.Matrix.res)), pixelSize)
+res
